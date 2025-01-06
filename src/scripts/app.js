5c601a7fdecc5d231ff93090a7914fe08b60acfc
Navegado <navegao@sample.xyz>
2025-01-06 08:04:59 -0400

    Add node cloning (without sucess, not animate to down)

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,10 @@
 const sortBtn = document.querySelector("#sortBtn")
 const unsortBtn = document.querySelector("#unsortBtn")
 const nodeNumbers = Array.from(document.querySelectorAll(".nodeNumber"))
+const nodeContainer = document.querySelector(".nodeContainer").cloneNode()
+const containerNodeContainer = document.querySelector(".containerNodeContainer")
+
+console.log(nodeContainer)
 
 const anim = {
     keyframeLeft: [{ transform: 'translate(-1rem, 2rem)' }],
@@ -38,19 +42,43 @@ function downRight(arr) {
     })
 }
 
+function cloneNodes(arrLeft, arrRight) {
+    let container1 = nodeContainer.cloneNode()
+    let container2 = nodeContainer.cloneNode()
+    
+    arrLeft.forEach(node => {
+        container1.appendChild(node.cloneNode(true))
+    })
+    arrRight.forEach(node => {
+        container2.appendChild(node.cloneNode(true))
+    })
+
+    containerNodeContainer.appendChild(container1)
+    containerNodeContainer.appendChild(container2)
+
+}
+
 function mS(arr) {
     if (arr.length < 2) return
 
-    const half = Math.ceil(arr.length / 2)
-    const left = arr.slice(0, half)
-    const right = arr.slice(half)
+    const arrCopy = []
 
+    arr.forEach(node => {
+        arrCopy.push(node.cloneNode(true))
+    })
+
+    const half = Math.ceil(arr.length / 2)
+    const left = arrCopy.slice(0, half)
+    const right = arrCopy.slice(half)
+
+    cloneNodes(left, right)
 
     downLeft(left)
     downRight(right)
 
-    mS(left)
-    mS(right)
+
+    //mS(left)
+    //mS(right)
 
     console.log(left)
     console.log(right)
