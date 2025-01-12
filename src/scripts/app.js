537254fcd4f35a382a168a5b8f5f4881ced156e9
Navegado <navegao@sample.xyz>
2025-01-12 19:51:51 -0400

    animation implemented with clientRect()

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,10 @@
 const sortBtn = document.querySelector("#sortBtn")
 const unsortBtn = document.querySelector("#unsortBtn")
+const animOpts = {
+    duration: 700,
+    fill: "forwards",
+    easing: "ease-in-out"
+}
 
 
 const parentContainerModel = document.querySelector(".containerContainer").cloneNode()
@@ -25,6 +30,8 @@ function nodeNumber(value) {
 }
 
 const defaultUnsortList = [4, 2, -1, 1]
+// const defaultUnsortList = [1, 2, 3, 4]
+// const defaultUnsortList = [4, 2, -1]
 
 defaultUnsortList.forEach(value => {
     parentContainer
@@ -42,16 +49,87 @@ function aux() {
     container.classList.toggle("translate-x-4")
 }
 
+function getXlen(elem1, elem2) {
+    const a = elem1.getBoundingClientRect().x
+    const b = elem2.getBoundingClientRect().x
+    const num = (b-a) / 16
+    return +num.toPrecision(4)
+}
+
 function timeout(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function chvalue(elem1, elem2) {
+    elem1.innerText = elem2.innerText
+}
+
+
+async function jS(containerLeft, containerRight, parentContainer, parentNodes) {
+    console.log(containerLeft)
+    console.log(containerRight)
+    const left = Array.from(containerLeft.querySelectorAll(".nodeNumber"))
+    const right = Array.from(containerRight.querySelectorAll(".nodeNumber"))
+    const length = parentNodes.length
+
+    console.log(
+        left,
+        right,
+        parentNodes
+    )
+
+    console.log(+left.at(0).innerText)
+    console.log(+right.at(0).innerText)
+    console.log(length)
+
+    for (let i = 0; i < length + 1; i++) {
+
+        if (left.length && right.length) {
+            if (+left.at(0).innerText < +right.at(0).innerText) {
+                const l = left.shift()
+                l.animate({transform: `translate(${getXlen(l, parentNodes[i])}rem, -${4}rem)`}, animOpts)
+                await timeout(700)
+                chvalue(parentNodes[i], l)
+            } else {
+                const r = right.shift()
+                r.animate({transform: `translate(${getXlen(r, parentNodes[i])}rem, -${4}rem)`}, animOpts)
+                await timeout(700)
+                chvalue(parentNodes[i], r)
+            }
+
+        } else {
+
+            if (left.length) {
+                const l = left.shift()
+                l.animate({transform: `translate(${getXlen(l, parentNodes[i])}rem, -${4}rem)`}, animOpts)
+                await timeout(700)
+                chvalue(parentNodes[i], l)
+            }
+            else if (right.length) {
+                const r = right.shift()
+                r.animate({transform: `translate(${getXlen(r, parentNodes[i])}rem, -${4}rem)`}, animOpts)
+                await timeout(700)
+                chvalue(parentNodes[i], r)
+            } else {
+                parentContainer.querySelector(".nodeArray").classList.remove("opacity-75")
+                containerLeft.remove()
+                containerRight.remove()
+            }
+        }
+
+    }
+
+    return parentContainer
+}
+
+    
+
 async function mS(container) {
     const nodes = Array.from(container.querySelector(".nodeArray").children)
-    if ( nodes.length < 2 ) return
+    if ( nodes.length < 2 ) return container
 
     const half = Math.ceil(nodes.length / 2)
-    console.log(half)
+    // console.log(half)
 
     
     container.querySelector(".nodeArrayChild").appendChild(parentContainerModel.cloneNode())
@@ -64,7 +142,7 @@ async function mS(container) {
     container.querySelector(".containerContainer").appendChild(rightContainer)
 
     nodes.forEach((node, i) => {
-        console.log(i, half)
+        // console.log(i, half)
         if (i < half)
             leftContainer.querySelector(".nodeArray").appendChild(node.cloneNode(true))
         else
@@ -75,14 +153,17 @@ async function mS(container) {
     container.querySelector(".nodeArray").classList.add("opacity-75")
     leftContainer.classList.remove("-translate-y-12")
     leftContainer.classList.add("-translate-x-4", "translate-y-4")
-    console.log(leftContainer)
+    //console.log(leftContainer)
     await timeout(700)
     rightContainer.classList.remove("-translate-y-12")
     rightContainer.classList.add("translate-x-4", "translate-y-4")
-    console.log(rightContainer)
+    //console.log(rightContainer)
     await timeout(700)
 
-    mS(leftContainer)
-    mS(rightContainer)
+    const containerLeft = await mS(leftContainer)
+    const containerRight = await mS(rightContainer)
+
+    const sortContainer = await jS(containerLeft, containerRight, container, nodes)
+    return sortContainer
     
 }
