624933656891947b777d8a1d3de73f8c2ff96653
Navegado <navegao@sample.xyz>
2025-10-26 20:57:51 -0400

    FEAT: relative nesting children

--- src/app.js
+++ src/app.js
@@ -8,6 +8,19 @@ mainCanvas.appendChild(msArray.create([1, 2, 3, 4]))
 sortBtn.onclick = () => {
     console.log("execute")
     console.log(msArray.divide())
+    
+    const { left, right } = msArray.divide()
+    const msLeft = new MsArray()
+    const msRight = new MsArray()
+
+    msLeft.create(left)
+    msRight.create(right)
+    msArray.childrenContainer.appendChild(msLeft.create())
+    msArray.childrenContainer.appendChild(msRight.create())
+
+    msLeft.move(false)
+    msRight.move(true)
+
 }
 
 unsortBtn.onclick = () => {
--- src/msArray.js
+++ src/msArray.js
@@ -1,17 +1,22 @@
 // store styles
 const styles = {
     array: "flex justify-center gap-8",
-    unit: "size-8 border-2 grid place-content-center"
+    relativeContainer: "relative flex justify-center",
+    childrenContainer: "absolute bottom-0 left-0 right-0",
+    unit: "size-8 border-2 grid place-content-center",
+    toDown: "translate-y-12",
+    toLeft: "-translate-x-8",
+    toRight: "translate-x-8"
 }
 
 class MsArray {
     constructor() {
         this.array = null
         this.element = null
-
+        this.childrenContainer = null
     }
     
-    create(arr) {
+    create(arr=null) {
         //early return element if array has created already
         if (this.array)
             return this.element
@@ -19,9 +24,25 @@ class MsArray {
         this.array = arr //assign array to the attribute
 
         this.element = document.createElement("ms-array")
-        this.element.className = styles.array
-
         
+        //create array elements container div
+        const parent = document.createElement("div")
+        //create array children container relative
+        const container = document.createElement("div")
+
+        //create array children container absolute
+        const childrenContainer = document.createElement("div")
+        //nest absolute children container into relative container
+        container.appendChild(childrenContainer)
+
+        //store absolute children container only
+        this.childrenContainer = childrenContainer
+
+        //set tailwindcss class styles
+        parent.className = styles.array
+        container.className = styles.relativeContainer
+        childrenContainer.className = `${styles.childrenContainer} ${styles.array}`
+
 
         arr.forEach(i => {
             // create each unit element
@@ -30,9 +51,12 @@ class MsArray {
             unit.innerText = i
             
             // append to the array
-            this.element.appendChild(unit)
+            parent.appendChild(unit)
     
         });
+
+        this.element.appendChild(parent)
+        this.element.appendChild(container)
         
         return this.element
     }
@@ -44,6 +68,12 @@ class MsArray {
         const right = this.array.slice(h)
 
         return { left, right }
+    }
+
+    move(to) {
+        //true = right , false = left, but ever to down
+        this.element.classList.add(styles.toDown)
+        to ? this.element.classList.add(styles.toRight) : this.element.classList.add(styles.toLeft)
     }
 }
 
